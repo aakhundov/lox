d75736b53229f1565286e71df2a8c826dc5fd4cf
Adnan Akhundov <adnan.akhundov@gmail.com>
2024-05-18 11:53:04 -0700

    Add for loop

--- jlox/Parser.java
+++ jlox/Parser.java
@@ -2,6 +2,7 @@ package jlox;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static jlox.TokenType.*;
 
@@ -54,6 +55,8 @@ class Parser {
       return printStatement();
     if (match(WHILE))
       return whileStatement();
+    if (match(FOR))
+      return forStatement();
     if (match(LEFT_BRACE))
       return new Stmt.Block(block());
 
@@ -81,6 +84,48 @@ class Parser {
     Stmt body = statement();
 
     return new Stmt.While(condition, body);
+  }
+
+  private Stmt forStatement() {
+    consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+    Stmt initializer;
+    if (match(SEMICOLON))
+      initializer = null;
+    else if (match(VAR))
+      initializer = varDeclaration();
+    else
+      initializer = expressionStatement();
+
+    Expr condition = null;
+    if (!check(SEMICOLON))
+      condition = expression();
+    consume(SEMICOLON, "Expect ';' after loop condition.");
+
+    Expr increment = null;
+    if (!check(RIGHT_PAREN))
+      increment = expression();
+    consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+
+    Stmt body = statement();
+
+    if (increment != null)
+      body = new Stmt.Block(
+          Arrays.asList(
+              body,
+              new Stmt.Expression(increment)));
+
+    if (condition == null)
+      condition = new Expr.Literal(true);
+    body = new Stmt.While(condition, body);
+
+    if (initializer != null)
+      body = new Stmt.Block(
+          Arrays.asList(
+              initializer,
+              body));
+
+    return body;
   }
 
   private Stmt printStatement() {
