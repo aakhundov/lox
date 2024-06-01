5672f12625dfc4630571dd901e58628d421dfd56
Adnan Akhundov <adnan.akhundov@gmail.com>
2024-06-01 13:54:40 -0700

    Add clox compiler

--- clox/debug.c
+++ clox/debug.c
@@ -6,11 +6,11 @@
 void disassembleChunk(Chunk *chunk, const char *name)
 {
   printf("== %s ==\n", name);
-
   for (int offset = 0; offset < chunk->count;)
   {
     offset = disassembleInstruction(chunk, offset);
   }
+  printf("\n");
 }
 
 int simpleInstruction(const char *name, int offset)
